94d3eeaf609a834834439b7a7b6624878d70c87f
Ken Mbuki <kmbuki@hypertechsolutions.co.ke>
2018-01-27 15:35:05 +0000

    config for pipeline

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8888/epic-world-limited/';
+$config['base_url'] = '';
 
+// http://localhost:8888/epic-world-limited/
+// 
+// 
 /*
 |--------------------------------------------------------------------------
 | Index File
